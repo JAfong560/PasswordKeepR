cfe3e3975afa311eb3c6faed4482ab71110d36f6
aliy0012 <alikamal415@yahoo.com>
2021-11-29 21:01:03 +0000

    Edit button jquerry structure-begining

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,3 +14,14 @@ function generatPassword() {
   }
   return randomString;
 }
+
+//***********edit button in main************************
+$(document).ready(function() {
+  $("#Edit").on("submit", function (event) {
+    event.preventDefault();
+
+    //here where to get the edit url/account
+    //and to post it back
+    //*********************************************
+  })
+});
