5f92a2675a3c55d99956e34b5c2cbe79ef2c03e7
aliy0012 <alikamal415@yahoo.com>
2021-11-29 20:45:38 +0000

    function for creating password

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,16 @@
 // Client facing scripts here
+
+
+//generating random password for the user
+function generatPassword() {
+  let randomString = "";
+  let characters =
+    "$%#@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 9; i++) {
+    randomString += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return randomString;
+}
