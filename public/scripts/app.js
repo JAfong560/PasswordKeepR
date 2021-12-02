5aa9009f2b6a19d64f59d754bcb576da04fc1a8d
Jonathan Afong <jonny560a@hotmail.com.com>
2021-12-02 13:08:32 -0500

    Implement copy button function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,10 +27,17 @@
 // });
 
 
-// //*****************creating new username/password */
-// $(doucment).ready(function(){
-//   $("create").click(function(){
+// //*****************copy password to clipboard*/
+$(document).ready(function(){
+  $(".cpButton").click(function(){
+    /* Get the text field */
+    let copyText = this;
 
-//   })
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
 
-// })
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  });
+
+})
